3203b6847ef9ff0e4d3e9da42fbd2f7f61ce2ef5
cassia <cgully01@gmail.com>
2022-08-02 17:09:35 +1000

    Added es-lint and prettier

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,28 @@
-import express, {Request, Response, Application} from 'express';
+import express, { Request, Response, Application } from 'express';
 import { findCat } from './store';
 
-const app : Application = express();
+const app: Application = express();
 const PORT = process.env.PORT || 3000;
 
 // base url
-app.get("/", (req: Request, res: Response) : void => {
-    res.status(404).send();
-  });
+app.get('/', (req: Request, res: Response): void => {
+  res.status(404).send();
+});
 
-app.get("/ping", (req: Request, res: Response) : void => {
-    res.send("pong");
-  });
+app.get('/ping', (req: Request, res: Response): void => {
+  res.send('pong');
+});
 
 // returning cats based on their id
-app.get("/cats/:id", (req: Request, res: Response) : void => {
-    let id: number = parseInt(req.params.id)
+app.get('/cats/:id', (req: Request, res: Response): void => {
+  const id: number = parseInt(req.params.id);
 
-    // filtering JSON data to find matching cat
-    const matchingCat = findCat(id);
-    
-    matchingCat ? res.send({name: matchingCat.name }) : res.status(404).send();
-  });
+  // filtering JSON data to find matching cat
+  const matchingCat = findCat(id);
 
-app.listen(PORT, () : void => {
-    console.log(`Server Running on https://localhost:${PORT}`);
+  matchingCat ? res.send({ name: matchingCat.name }) : res.status(404).send();
+});
+
+app.listen(PORT, (): void => {
+  console.log(`Server Running on https://localhost:${PORT}`);
 });
